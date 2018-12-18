e3bed0ce1f96c300cb31f726163c28356015a00b
Ty Mazey <tymazey@gmail.com>
2018-12-18 09:24:15 -0700

    adds exhibit test

---
+++ test/museum_test.rb
@@ -0,0 +1,50 @@
+require_relative 'test_helper'
+
+class MuseumTest < Minitest::Test
+
+  def test_it_exsist
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_instance_of Museum, dmns
+  end
+
+  def test_it_has_name
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+  end
+
+  def test_it_starts_with_no_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal [], dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    imax = Exhibit.new("IMAX", 15)
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+  end
+
+  def test_it_can_recommend_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    imax = Exhibit.new("IMAX", 15)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    bob = Patron.new("Bob", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+
+    assert_equal [], exhibit.recommend_exhibits(bob)
+  end
+end
